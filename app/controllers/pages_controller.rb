f31a54fbd770ec06e7b21140e83491a0441ff5d0
Beth Giobbe <bgiobbe@gmail.com>
2013-04-03 13:13:06 -0400

    Added layout for header and content and stubs for non-db related pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+  def index
+    render :layout => false
+  end
+  
+  def interests
+  end
+
+  def resume
+  end
+  
+  def vision
+  end
+  
+end
